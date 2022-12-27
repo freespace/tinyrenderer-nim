6e02e14391295de7dcc2879add4389904281cd40
Shuning Bian <freespace@gmail.com>
2022-12-27 19:41:55 +1100

    Working 1st pass at a PAM library

---
+++ src/pam.nim
@@ -0,0 +1,52 @@
+import std/strformat
+
+# expected to be RGBA
+type Pixel* = array[4, uint8]
+
+# W, H: static[int] declares W and H to be integer type parameters which must be constant
+# expressions. The latter is enforced via static[]. Note that the image is stored row-major
+# so image[0] is the first row, i.e. indexing is (y, x) not (x, y). For this reason
+# it is strongly suggested to use set_pixel() to set colours as it will translate the (x, y)
+# into (col, row)
+type Image*[W, H: static[uint]] = array[H, array[W, Pixel]]
+
+proc set_pixel*(image: var Image, x: uint16, y: uint16, pixel: Pixel): void = 
+  image[y][x] = pixel
+
+proc write_image*(image: Image, output_path: string): void =
+  let out_fh = io.open(output_path, FileMode.fmWrite)
+
+  # write out the ascii header starting with magic that identifies this as a binary
+  # PPM image
+  io.write(out_fh, "P7\n")
+
+  # ... then the image dimensions
+  const width_str = &"WIDTH {len(image[0])}\n"
+  const height_str = &"HEIGHT {len(image)}\n"
+
+  io.write(out_fh, width_str)
+  io.write(out_fh, height_str)
+
+  # ... then the number of channels, always 4 for RGBA
+  io.write(out_fh, "DEPTH 4\n")
+
+  # ...  then the maximum value, which is 255 as we are using 1 byte per channel
+  io.write(out_fh, "MAXVAL 255\n")
+
+  # ... then the pixel type
+  io.write(out_fh, "TUPLTYPE RGB_ALPHA\n")
+
+  # ... and signal end of the header
+  io.write(out_fh, "ENDHDR\n")
+
+  # now write out the image, one pixel at a time. First the red value, then green and finally blue.
+  # Note that we write bottom up, so the image is flipped with origin at bottom-left instead of
+  # top-left
+  for rdx in countdown(high(image), 0):
+    for cdx in 0..high(image[0]):
+      let pixel = image[rdx][cdx]
+      if io.writeBytes(out_fh, pixel, 0, len(pixel)) != len(pixel):
+        raise newException(IOError, "Fewer than expected number of bytes written")
+
+  io.close(out_fh)
+
---
+++ tests/test_pam.nim
@@ -0,0 +1,18 @@
+import ../src/pam
+
+# nim 0-init's all storage, so now we have a bunch of 0s
+const width = 128
+const height = 64
+var im: pam.Image[width, height]
+
+# origin at bottom left is red
+pam.set_pixel(im, 0, 0, [255u8, 0, 0, 255])
+
+# opposing corner is green
+pam.set_pixel(im, width-1, height-1, [0u8, 255, 0, 255])
+
+# centre is blue
+pam.set_pixel(im, width div 2, height div 2, [0u8, 0, 255, 255])
+
+# to view the image: convert /tmp/test.pam /tmp/test.png && open /tmp/test.png
+pam.write_image(im, "/tmp/test.pam")
